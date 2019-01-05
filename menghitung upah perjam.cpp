48a987bd5a368b3a01d81a10a1e964f814641598
nurasiyah <45659380+nurasiyah@users.noreply.github.com>
2019-01-05 15:08:04 +0700

    Add files via upload

---
+++ menghitung upah perjam.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int j,hasil;
+    cout <<"Program menghitung upah perjam\n";
+    cout <<"==============================\n";
+    cout <<"Masukkan jumlah jam ="; cin>>j;
+    hasil = j*5000;
+    cout <<"Jumlah upah keseluruhan =" <<hasil;
+    return 0;
+}
